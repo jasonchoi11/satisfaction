8a5c8036fe33113b0a0d1b3e510dbb39873f7ffc
jasonchoi11 <hsloveme1@naver.com>
2020-07-16 16:31:59 +0900

    .

---
+++ src/main/java/rental_private/external/Call.java
@@ -0,0 +1,14 @@
+package rental_private.external;
+
+public class Call {
+
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+}
---
+++ src/main/java/rental_private/external/CallService.java
@@ -0,0 +1,17 @@
+
+package rental_private.external;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.Date;
+
+@FeignClient(name="Call", url="http://Call:8080")
+public interface CallService {
+
+    @RequestMapping(method= RequestMethod.POST, path="/calls")
+    public void callSurvey(@RequestBody Call call);
+
+}
